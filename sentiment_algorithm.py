7d96fb1ae2e94ca396b337beb5f82b7aee7e1b43
Lekshmi <vijayalekshmivr203@gmail.com>
2024-09-03 15:43:18 +0100

    Changes made for finding sentiment for just a amplifier/negation

--- sentiment_algorithm.py
+++ sentiment_algorithm.py
@@ -3,7 +3,12 @@ import re
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk import sent_tokenize
-from translator import translate_text
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+#from translator import translate_text
+
+label_encoder = LabelEncoder()
+
 
 
 
@@ -12,15 +17,19 @@ class SentimentAlgorithm:
         self.custom_lexicon = {
             # Positive sentiment words
             "good": 2,
+            "nice":2,
             "happy": 3,
+            "Big":3,
             "pleasant": 3,
             "suprisingly": 3,
             "great": 4,
             "fantastic": 4,
             "love": 4,
             "joyful":4,
+            "congratulation":4,
             "delightful": 4,
             "strong":4,
+            "awesome":4.75,
             "excellent": 5,
             "wonderful": 5,
             "amazing": 5,
@@ -32,49 +41,91 @@ class SentimentAlgorithm:
 
             # Negative sentiment words
             "bad": -2,
+            "mean":-2,
             "sad": -3,
+            "Small":-3,
+            "problem":-3, "problems":-3,
             "disappointing": -3,
             "unpleasant": -3,
+            "abusive":-4,
             "terrible": -4,
             "awful": -4,
             "miserable": -4,
             "hate": -4,
             "low":-4,
             "weak":-4,
+            "ruin":-4,
+            "worse":-4.75,
             "horrible": -5,
             "disgusting": -5,
             "negative":-5,
+            "violence":-5,
+
             
         }
         
         self.custom_amplifiers = {
-            # Modifiers (amplifiers/diminishers)
+            # Modifiers (amplifiers)
             # Amplifier Increases sentiment score, the value will be greater than 0.
-            # Diminishers: Decreases the sentiment score, value between 0 & 1
-            "barely": 0.3,
-            "slightly": 0.5,
-            "somewhat": 0.7,
-            "true": 0.8,
-            "quite": 1.2,
-            "very": 1.5,
-            "right":1.5,
-            "flavourful":1.5,
-            "extremely": 2,
-            "recommend":2,
+            "exact":0.5, "excatly":0.5,
+            "barely": 0.10,
+            "slightly": 0.10,
+            "somewhat": 0.10,
+            "unlimited":0.75,
+            "normal":0.5,
+            "new":0.55,
+            "true": 0.8, "truly":0.8,
+            "even":0.55, "evenly":0.55,          
+            "attentive":0.8,
+            "more":0.85,
+            "compliments":0.9,
+            "win":0.9, "winning":0.95, "wins":0.99,
+            "easy":0.35,
+            "fortunately":0.5, "fortunate":0.5,
+            "glad":0.8,
+            "enjoy":0.85, "enjoyable":0.85, "enjoying":0.85, "enjoyed":0.85,
+            "excite":0.95, "excited":0.95, "exciting":0.95,
+            "protect":0.75, "protected":0.75, "protecting":0.75, "protects":0.75,
+            "quite": 0.25,
+            "very": 0.65,
+            "right":0.8,
+            "flavourful":0.8, "flavour":0.45,
+            "praise":0.75, "applause":0.75,
+            "motivate":0.90, "motivates":0.90, "motivated":0.90, "motivating":0.90,
+            "extremely": 0.75,
+            "recommend":0.8, "recommended":0.8, "recommending":0.8,
             
             
         }
 
         self.custom_negations ={
             # Negations (Negates the sentiment of the following word, the value will be less than 0)
+            "different":-0.25, "difference":-0.25,
+            "left":-0.5,
+            "leave":-0.5,
+            "miss":-0.55, "missing":-0.75, "missed":-0.75,
+            "old":-0.55,
+            "bored":-0.6, "bore":-0.6, "boring":-0.6,
             "hardly": -0.7,
+            "lose":-0.75, "losing":-0.95, "losed":-0.75, 
             "barely": -0.8,
+            "enough":-0.75,
+            "uneven":-0.8,
             "False":-0.8,
-            "not": -1,
-            "never": -1,
-            "no": -1,
-            "wrong":-1,
-        }
+            "distractive":-0.8,
+            "hard":-0.9,
+            "fail":-0.9, "fails":-0.9, "failed":-1.75,
+            "unfortunately":-0.95, "unfortunate":-0.95,
+            "don't":-0.99,
+            "not": -0.99,
+            "never": -0.99,
+            "no": -0.99,
+            "wrong":-0.85,
+            "curse":-0.9,
+            "insult":-0.9,
+            "harras":-0.9, "harrasing":-0.9,            
+        }            
+
 
 
 
@@ -114,6 +165,37 @@ class SentimentAlgorithm:
         else:
             raise ValueError("Input data must be a pandas DataFrame or a string representing a paragraph.")
 
+    '''def calculate_sentiment(self, text):
+        words = self.preprocess(text)
+        score = 0
+        i = 0
+        
+        while i < len(words):
+            word = words[i]
+            if word in self.custom_lexicon:
+                current_score = self.custom_lexicon[word]
+                
+                # Check for modifiers or negations before the sentiment word
+                if i > 0:
+                    prev_word = words[i-1]
+                    if prev_word in self.custom_negations:
+                        current_score *= self.custom_negations[prev_word]
+                    elif prev_word in self.custom_amplifiers:
+                        current_score *= self.custom_amplifiers[prev_word]
+                    
+                    # Handle the case where negation and amplifier are combined
+                    if i > 1:
+                        prev_prev_word = words[i-2]
+                        if prev_prev_word in self.custom_negations:
+                            current_score *= self.custom_negations[prev_prev_word]
+                            
+                        elif prev_prev_word in self.custom_amplifiers:
+                            current_score *= self.custom_amplifiers[prev_prev_word]
+                            
+                score += current_score
+            i += 1
+        
+        return score'''
     def calculate_sentiment(self, text):
         words = self.preprocess(text)
         score = 0
@@ -131,17 +213,29 @@ class SentimentAlgorithm:
                         current_score *= self.custom_negations[prev_word]
                     elif prev_word in self.custom_amplifiers:
                         current_score *= self.custom_amplifiers[prev_word]
-                        
+                    
                     # Handle the case where negation and amplifier are combined
                     if i > 1:
                         prev_prev_word = words[i-2]
                         if prev_prev_word in self.custom_negations:
                             current_score *= self.custom_negations[prev_prev_word]
-
+                        elif prev_prev_word in self.custom_amplifiers:
+                            current_score *= self.custom_amplifiers[prev_prev_word]
+                            
                 score += current_score
+            
+            # Handle the case where amplifiers or negations are alone
+            elif word in self.custom_amplifiers or word in self.custom_negations:
+                if i == len(words) - 1 or (i < len(words) - 1 and words[i+1] not in self.custom_lexicon):
+                    if word in self.custom_negations:
+                        score += self.custom_negations[word]  # This could be negative or a small positive number
+                    elif word in self.custom_amplifiers:
+                        score += self.custom_amplifiers[word]  # This could increase or emphasize the current score
+                        
             i += 1
         
         return score
+
 
 
     def classify_sentiment(self, text):
@@ -169,3 +263,41 @@ class SentimentAlgorithm:
             sentiment_labels.append((sentence, label))
         
         return sentiment_scores, sentiment_labels
+    
+    def fit(self, X_train, y_train):
+        if len(X_train) != len(y_train):
+            raise ValueError("Training features and labels must have the same length.")
+        
+        for text, sentiment in zip(X_train, y_train):
+            words = self.preprocess(text)
+            for word in words:
+                if word not in self.custom_lexicon:
+                    # Assign sentiment value to the new word
+                    if sentiment == 1:  # Assuming 1 is for 'positive'
+                        self.custom_lexicon[word] = 1
+                    elif sentiment == -1:  # Assuming -1 is for 'negative'
+                        self.custom_lexicon[word] = -1
+                    elif sentiment == 0:  # Assuming 0 is for 'neutral'
+                        self.custom_lexicon[word] = 0
+
+    def transform(self, X):
+        # Transform input data to the form needed for prediction
+        return [self.preprocess_paragraph(text) for text in X]
+
+    def predict(self, X_test):
+        # Use the transform method to preprocess test data
+        cleaned_texts = [' '.join(text) for text in self.transform(X_test)]
+        predicted_labels = [self.classify_sentiment(text) for text in cleaned_texts]
+        return predicted_labels
+    
+    def compute_accuracy(self, X_test, y_test):
+        predicted_labels = self.predict(X_test)
+        accuracy = accuracy_score(y_test, predicted_labels)
+        return accuracy
+
+    '''def compute_accuracy(self, X_test, y_test):
+        # Ensure that the y_test labels are in the same format as predictions
+        y_test_labels = [label_encoder.inverse_transform([label])[0] for label in y_test]
+        predicted_labels = self.predict(X_test)
+        accuracy = accuracy_score(y_test_labels, predicted_labels)
+        return accuracy'''
